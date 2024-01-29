cdf99237cf71c3542ae8db293841c6b8b3de5ff4
Ben Frame <brewerytattooer@gmail.com>
2024-01-29 14:52:43 -0500

    wip

--- data/seeds/02-make-recipes.js
+++ data/seeds/02-make-recipes.js
@@ -30,7 +30,7 @@ const steps = [
     {step_text: 'Add broccoli', step_number: 2, recipe_id: 1},
     {step_text: 'Add pesto mixed with pasta', step_number: 3, recipe_id: 1},
     //Lemon Chicken
-    {step_text: 'Heat ovne', step_number: 1, recipe_id: 2},
+    {step_text: 'Heat oven', step_number: 1, recipe_id: 2},
     {step_text: 'Put chicken and lemon in oven', step_number: 2, recipe_id: 2},
     {step_text: 'Put in oven at 500 degrees', step_number: 3, recipe_id: 2},
     //Salmon en Papillote
